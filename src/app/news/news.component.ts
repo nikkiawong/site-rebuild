acce20d0c16543cacf13bbb8e90bc369e649fe7f
Nikki Wong <nikkiwong@miami.edu>
2018-10-26 16:17:55 -0700

    add some content and styling to news page

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { RecentPost } from '../models/recentPost.model';
 
 @Component({
   selector: 'app-news',
@@ -11,5 +12,9 @@ export class NewsComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  newsPosts: RecentPost[] = [
+    new RecentPost("10 Immersive Rooms at “Mickey: The True Original Exhibition” Have Been Announced", "We can't wait to see them!", "https://ohmy.disney.com/wp-content/uploads/2018/08/Mickey-The-True-Original-Exhibition-242x141.jpg", "https://ohmy.disney.com/news/2018/10/26/mickey-the-true-original-exhibition-immersive-rooms/")
+  ]
 
 }

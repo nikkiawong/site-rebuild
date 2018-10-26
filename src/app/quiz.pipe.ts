0b176c7912685e3f9279e1420d6f4581fe52dc4d
Nikki Wong <nikkiwong@miami.edu>
2018-10-26 11:22:55 -0700

    style retail posts

---
+++ src/app/quiz.pipe.ts
@@ -0,0 +1,19 @@
+// import {Pipe, PipeTransform} from '@angular/core';
+// import { PostService } from '../post.service';
+// import { FirebaseListObservable } from 'angularfire2/database';
+//
+// @Pipe({
+//
+// })
+//
+// export class QuizPipe implements PipeTransform {
+//   transform(input: posts){
+//     var output: posts = [];
+//     for (var i = 0; i < input.length; i++) {
+//       if (input[i].done === false) {
+//         output.push(input[i]);
+//       }
+//     }
+//     return output;
+//   }
+// }
